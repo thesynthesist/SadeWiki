977f295df249570c45f18ba9edb4a6438913115c
Azz <azz@hinkley.dev>
2025-05-17 23:04:11 +0000

    TEST: Write base of unit tests (#4)

---
+++ tests/test_unit.py
@@ -0,0 +1,25 @@
+class TestStatus:
+    def test_status_time(self):
+        """
+        Confirm the function waits a moment when it encounters a rate limit response
+        """
+        assert False # TODO
+
+    def test_status_error(self):
+        """
+        Confirm the function raises an error when a non 200 code is returned
+        """
+        assert False # TODO
+
+class TestAuthentication:
+    def test_authentication_success(self):
+        """
+        Confirm the system authenticates correctly when using the environment variable token
+        """
+        assert False # TODO
+
+    def test_authentication_failure(self):
+        """
+        Confirm the system fails when using a fake token
+        """
+        assert False # TODO
